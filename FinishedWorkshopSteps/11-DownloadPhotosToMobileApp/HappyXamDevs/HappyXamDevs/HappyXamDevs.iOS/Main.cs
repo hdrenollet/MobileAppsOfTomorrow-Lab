345d9df6c9d6dcaeee99c517dfabfeada2b9a953
Howard Renollet <Howard.Renollet@comfortsystemsusa.com>
2019-03-03 10:10:05 -0800

    test

--- FinishedWorkshopSteps/11-DownloadPhotosToMobileApp/HappyXamDevs/HappyXamDevs/HappyXamDevs.iOS/Main.cs
+++
@@ -1,9 +0,0 @@
-﻿using UIKit;
-
-namespace HappyXamDevs.iOS
-{
-    public class Application
-    {
-        static void Main(string[] args) => UIApplication.Main(args, null, nameof(AppDelegate));
-    }
-}
